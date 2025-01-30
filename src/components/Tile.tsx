9238272a6809c0a7a1ed728c7415a918ee9dae51
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-30 13:07:24 +0300

    upd: gsap optimization revert

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,8 +1,9 @@
+import { memo, useEffect, useRef } from "react";
+
 import { Coordinates } from "../types";
 import { TURN_ANIMATION_DURATION } from "../constants";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { useRef } from "react";
 
 const turnAnimationDuration = TURN_ANIMATION_DURATION / 1000;
 
@@ -12,81 +13,94 @@ type TileProps = {
 	coordinates: Coordinates;
 };
 
-export const Tile: React.FC<TileProps> = (props) => {
-	const boxRef = useRef(null);
-
-	useGSAP(
-		() => {
-			const color = getComputedStyle(document.documentElement).getPropertyValue(`--tile-${props.power}`);
+export const Tile: React.FC<TileProps> = memo(
+	(props) => {
+		const boxRef = useRef(null);
+		useEffect(() => {
 			gsap.set(boxRef.current, {
 				x: props.coordinates.x,
 				y: props.coordinates.y,
-				backgroundColor: color,
-				boxShadow: `0 0 ${Math.floor(props.power)}px 1px ${color}`,
-				width: props.size,
-				height: props.size,
-				display: "block",
 			});
-		},
-		{ dependencies: [], scope: boxRef }
-	);
-
-	useGSAP(
-		() => {
-			const color = getComputedStyle(document.documentElement).getPropertyValue(`--tile-${props.power}`);
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, []);
+		useGSAP(() => {
 			gsap.timeline()
 				.to(boxRef.current, {
 					scale: 1.1,
-					duration: 0.1,
+					duration: 0.2,
 				})
 				.to(boxRef.current, {
 					scale: 1,
 					duration: 0.1,
 				});
-			gsap.to(boxRef.current, {
-				backgroundColor: color,
-				boxShadow: `0 0 ${Math.floor(props.power)}px 1px ${color}`,
-				duration: 0.2,
-			});
-		},
-		{ dependencies: [props.power], scope: boxRef }
-	);
-
-	useGSAP(
-		() => {
-			const coordinates = props.coordinates;
-			const startX = Number(gsap.getProperty(boxRef.current, "x"));
-			const startY = Number(gsap.getProperty(boxRef.current, "y"));
-			if (startX !== coordinates.x) {
-				gsap.to(boxRef.current, {
-					x: coordinates.x,
-					duration: turnAnimationDuration,
-				});
-			}
-			if (startY !== coordinates.y) {
-				gsap.to(boxRef.current, {
-					y: coordinates.y,
-					duration: turnAnimationDuration,
-				});
-			}
-		},
-		{ dependencies: [props.coordinates], scope: boxRef }
-	);
-
-	return (
-		<div className={`absolute rounded-xl backdrop-blur-sm opacity-95 hidden`} ref={boxRef}>
-			{props.power >= 11 && (
-				<div className="top-0 right-0 bottom-0 left-0 absolute bg-transparent rounded-xl animate-epic-shadow" />
-			)}
-			<span
-				style={{
-					lineHeight: `${props.size}px`,
-					fontSize: `${props.size / 2 - props.power}px`,
-				}}
-				className="bg-clip-text bg-gradient-to-br from-white to-gray-300 font-bold text-transparent animate-pulse-slow"
-			>
-				{2 ** props.power}
-			</span>
-		</div>
-	);
-};
+		}, [props.power]);
+		useGSAP(
+			() => {
+				const coordinates = props.coordinates;
+				const startX = Number(gsap.getProperty(boxRef.current, "x"));
+				const startY = Number(gsap.getProperty(boxRef.current, "y"));
+				if (startX !== coordinates.x) {
+					gsap.to(boxRef.current, {
+						x: coordinates.x,
+						duration: turnAnimationDuration,
+					});
+				}
+				if (startY !== coordinates.y) {
+					gsap.to(boxRef.current, {
+						y: coordinates.y,
+						duration: turnAnimationDuration,
+					});
+				}
+			},
+			{ dependencies: [props.coordinates], scope: boxRef }
+		);
+		return (
+			<>
+				<div
+					className={`absolute rounded-xl backdrop-blur-sm opacity-95`}
+					ref={boxRef}
+					style={{
+						width: props.size,
+						height: props.size,
+					}}
+				>
+					{props.power >= 11 && (
+						<div
+							className="absolute bg-transparent rounded-xl animate-epic-shadow"
+							style={{
+								width: props.size,
+								height: props.size,
+							}}
+						/>
+					)}
+					<div
+						className="top-0 right-0 bottom-0 left-0 absolute flex justify-center items-center rounded-xl"
+						style={{
+							backgroundColor: `var(--tile-${props.power})`,
+							boxShadow: `0 0 ${Math.floor(props.power)}px 1px var(--tile-${props.power})`,
+							transition: "background-color 0.1s ease-in-out, box-shadow 0.1s ease-in-out",
+							willChange: "background-color, box-shadow",
+						}}
+					>
+						<span
+							style={{
+								lineHeight: `${props.size}px`,
+								fontSize: `${props.size / 2 - props.power}px`,
+							}}
+							className="bg-clip-text bg-gradient-to-br from-white to-gray-300 font-bold text-transparent animate-pulse-slow"
+						>
+							{2 ** props.power}
+						</span>
+					</div>
+				</div>
+			</>
+		);
+	},
+	(prevProps, nextProps) => {
+		return (
+			prevProps.power === nextProps.power &&
+			prevProps.coordinates.x === nextProps.coordinates.x &&
+			prevProps.coordinates.y === nextProps.coordinates.y
+		);
+	}
+);
